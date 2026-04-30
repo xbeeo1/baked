5c1936639e9913c6716a0d6e9fe1062bb9ae91bd
zaheer-xbeeo <zaheer.jamil@xbeeo.com>
2026-05-01 01:13:24 +0500

    fixed issue

--- cst_default_customer_pos/static/src/js/pos_order.js
+++ cst_default_customer_pos/static/src/js/pos_order.js
@@ -6,10 +6,13 @@ import { patch } from "@web/core/utils/patch";
 patch(PosOrder.prototype, {
     setup() {
         super.setup(...arguments);
-        const defaultPartner = this.config.default_customer;
-
-        if (defaultPartner && !this.finalized && !this.getPartner()) {
-            this.setPartner(defaultPartner);
+        const defaultPartner = this.config?.default_customer;
+        if (defaultPartner?.id && !this.finalized && !this.getPartner()) {
+            try {
+                this.setPartner(defaultPartner);
+            } catch (error) {
+                console.warn("Default partner could not be set:", error);
+            }
         }
     },
 });
