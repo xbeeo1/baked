5bb8118d620d1029cfc40bef125fcd771aa6f1d4
zaheer-xbeeo <zaheer.jamil@xbeeo.com>
2026-05-14 21:40:00 +0500

    Add files via upload
    remove  custom dashboard work

--- cno_baked_custom/__manifest__.py
+++ cno_baked_custom/__manifest__.py
@@ -15,14 +15,13 @@
 
     'website': 'https://cyngro.com',
 
-    "depends": ['base','stock','product','account','mrp','point_of_sale','spreadsheet_dashboard'],
+    "depends": ['base','stock','product','account','mrp','point_of_sale'],
 
     "data": [
         'views/product_template_views.xml',
         'views/product_variant_views.xml',
         'views/stock_views.xml',
         'views/pos_payment_method_views.xml',
-        'data/pos_dashboard_vendor.xml',
     ],
 
 }
